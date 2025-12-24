628ffc189e5699f387a612800fc8b0b033309083
Aviv Komemi <komemiav@gmail.com>
2025-12-24 16:17:15 +0200

    Main Menu
    Added level selection menu and level selection button prefabs and scripts
    Made main menu canvas into a prefab to reduce future git changes on main menu scene

---
+++ Assets/Scripts/UI/LevelSelectButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using Models.ScriptableObjects;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    /// <summary>
+    /// Component for handling a level selection button;
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class LevelSelectButton : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text levelNameText;
+        
+        public void SetSceneData(SceneSelectionData data)
+        {
+            levelNameText.text = data.SceneDisplayName;
+            
+            Button button = GetComponent<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => LoadLevel(data.Scene));
+        }
+
+        private void LoadLevel(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
--- Assets/Scripts/UI/LevelSelector.cs
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Models;
+using Models.ScriptableObjects;
+using UnityEngine;
 
 namespace UI
 {
@@ -7,6 +9,21 @@ namespace UI
     /// </summary>
     public class LevelSelector : MonoBehaviour
     {
-        
+        [SerializeField] private RectTransform levelButtonsLayoutParent;
+        [SerializeField] private LevelSelectButton levelButtonPrefab;
+        [SerializeField] private SceneSelectionData[] levels;
+
+        private void Start()
+        {
+            GenerateLevelButtons();
+        }
+        private void GenerateLevelButtons()
+        {
+            foreach (SceneSelectionData levelData in levels)
+            {
+                LevelSelectButton levelButton = Instantiate(levelButtonPrefab, levelButtonsLayoutParent);
+                levelButton.SetSceneData(levelData);
+            }
+        }
     }
 }
