ad7e598a40234e48617b7729c1933218268cce7b
Aviv Komemi <komemiav@gmail.com>
2025-12-25 11:27:57 +0200

    Player Audio
    Added missing handling for player audio on taking damage

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,18 @@
+﻿using Controllers;
+using UnityEngine;
+
+namespace Player
+{
+    /// <summary>
+    /// Component for handling player audio and sound effects.
+    /// </summary>
+    public class PlayerAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioClip damageTakenSoundEffect;
+
+        public void PlayDamageTakenSoundEffect()
+        {
+            AudioManager.Instance.PlaySoundEffect(damageTakenSoundEffect);
+        }
+    }
+}
